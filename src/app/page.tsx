531db407843ece29fc2971fd25402b150cb488a7
k2core <7179062@gmail.com>
2023-06-21 08:55:15 +0900

    피쳐된 포스트 보여주기(1차)
    엘리의 실수도 있고, 아무튼 featured posts 보여주기
    추가로 2차와 스타일링이 필요하다.
    
    Changes to be committed:
    modified:   src/app/page.tsx
    new file:   src/components/FeaturedPosts.tsx
    new file:   src/components/PostsGrid.tsx
    new file:   src/service/posts.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedPosts from "@/components/FeaturedPosts";
 import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts />
     </>
   );
 }
