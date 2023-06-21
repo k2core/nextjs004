30aee7671ed76f12bdb47d2d8bcadf9f1e9a49e9
k2core <7179062@gmail.com>
2023-06-21 14:11:19 +0900

    피쳐된 포스트 보여주기(스타일링) 완성
    스타일링까지 모두 완성함.
    
    Changes to be committed:
    modified:   src/components/FeaturedPosts.tsx
    modified:   src/components/PostCard.tsx
    modified:   src/components/PostsGrid.tsx

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -5,7 +5,7 @@ export default async function FeaturedPosts() {
   const posts = await getFeaturedPosts();
   return (
     <section>
-      <h2 className="text-2xl font-bold">Featured Posts</h2>
+      <h2 className="text-2xl font-bold my-2">Featured Posts</h2>
       <PostsGrid posts={posts} />
     </section>
   );
--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -4,7 +4,7 @@ import PostCard from "./PostCard";
 type Props = { posts: Post[] };
 export default function PostsGrid({ posts }: Props) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
         <li key={post.path}>
           <PostCard post={post} />
