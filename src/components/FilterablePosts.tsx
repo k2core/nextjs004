6333ebacae1edb13913ede388c9f9ea032ef8dad
k2core <7179062@gmail.com>
2023-06-24 11:23:34 +0900

    필터가능한 포스트 페이지(1) - 구현
    일단 구현부터 한다(스타일링은 뒤에).
    FilterablePosts 컴포넌트를 구현하고,
    그 안에 Categories 컴포넌트를 구현한다.
    
    Changes to be committed:
    modified:   src/app/posts/page.tsx
    new file:   src/components/Categories.tsx
    new file:   src/components/FilterablePosts.tsx

---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Post } from "@/service/posts";
+import { useState } from "react";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+const ALL_POSTS = "All Posts";
+
+export default function FilterablePosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+
+  return (
+    <section>
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}
