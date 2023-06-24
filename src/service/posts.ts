d3cc7f2a5199edaa30a0558e73dca1848ef6b5a9
k2core <7179062@gmail.com>
2023-06-24 13:58:02 +0900

    블로그 포스트 페이지
    각 포스트 파일을 보여주는 것을 구현한다.
    아직 마크다운 뷰어는 사용하지 않는다.
    
    Changes to be committed:
    new file:   src/app/posts/[slug]/page.tsx
    modified:   src/service/posts.ts

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,6 +10,8 @@ export type Post = {
   featured: boolean;
 };
 
+export type PostData = Post & { content: string };
+
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts() //
     .then((posts) => posts.filter((post) => post.featured));
@@ -25,4 +27,15 @@ export async function getAllPosts(): Promise<Post[]> {
   return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const metadata = await getAllPosts() //
+    .then((posts) => posts.find((post) => post.path === fileName));
+  if (!metadata)
+    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
 }
