1af03a801bad8cea14d58017949fc9114fc287b5
k2core <7179062@gmail.com>
2023-06-25 22:37:43 +0900

    마크다운 뷰어(3) - 코드문법 (완성)
    1. syntax highlight를 한다.
    2. react-syntax-highlighter 설치하고, ReactMarkdown 컴포넌트 사용
    3. 관련 TypeScript의 타입 추가 설치
    4. 외부 사이트 이미지를 위한 next.config.js 추가
    
    Changes to be committed:
    modified:   next.config.js
    modified:   package.json
    modified:   src/components/MarkdownViewer.tsx
    modified:   yarn.lock

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,9 +1,44 @@
+"use client";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={materialDark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+        img: (image) => (
+          <Image
+            className="w-full max-h-60 object-cover"
+            src={image.src || ""}
+            alt={image.alt || ""}
+            width={500}
+            height={350}
+          />
+        ),
+      }}
+      className="prose max-w-none"
+      remarkPlugins={[remarkGfm]}
+    >
       {content}
     </ReactMarkdown>
   );
