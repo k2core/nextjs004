0767bdbf60f623d3d298db25e6de7da4ed7867d0
k2core <7179062@gmail.com>
2023-06-27 08:56:34 +0900

    정보, 연락 페이지(2) - 입력폼
    입력폼을 작성한다.
    
    Changes to be committed:
    modified:   src/components/ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,3 +1,62 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
+
 export default function ContactForm() {
-  return <></>;
+  const [form, setForm] = useState<Form>({
+    from: "",
+    subject: "",
+    message: "",
+  });
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="from">Your Email</label>
+        <input
+          type="email"
+          id="from"
+          name="from"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+        />
+        <label htmlFor="subject">Subject</label>
+        <input
+          type="text"
+          id="subject"
+          name="subject"
+          required
+          value={form.subject}
+          onChange={onChange}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
