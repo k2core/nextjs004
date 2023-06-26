92001656e309f40d4033281d2e2564f74808f109
k2core <7179062@gmail.com>
2023-06-26 17:08:16 +0900

    정보, 연락 페이져(1) - 페이지
    정보(about), 연락(contact) 페이지 만들기
    이메일 입력폼은 다음에.
    
    Changes to be committed:
    modified:   src/app/about/page.tsx
    modified:   src/app/contact/page.tsx
    new file:   src/components/ContactForm.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,30 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <p>나에대해!</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 풀스택 개발자 <br />
+          사람과 디자인을 담는 웹앱을 만들고 있음
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          구글러 (~Now) <br />
+          메이스북 (~2019) <br />
+          삼준전자 (~2015)
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React, Vue, Node <br />
+          Git, Clean Code <br />
+          VS Code, IntelliJ, MongoDB
+        </p>
+      </section>
+    </>
+  );
 }
