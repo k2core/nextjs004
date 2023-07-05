6781c55d0be7c6bfc564e8698a76e70be13fd503
k2core <7179062@gmail.com>
2023-07-05 14:34:12 +0900

    이메일 보내기(1) - 클라이언트
    nodemailer, yup 설치
    클라이언트에서 메일 작성폼,
    api 요청 함수
    api 처리 컴포넌트 개발
    이후 서버 모듈(메일 실제 발송) 개발
    
    Changes to be committed:
    modified:   package.json
    new file:   src/app/api/contact/route.ts
    modified:   src/components/ContactForm.tsx
    new file:   src/service/contact.ts
    new file:   src/service/email.ts
    modified:   yarn.lock

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEvent, FormEvent, useState } from "react";
 import Banner, { BannerData } from "./Banner";
+import { sendContactEmail } from "@/service/contact";
 
 type Form = {
   from: string;
@@ -9,12 +10,14 @@ type Form = {
   message: string;
 };
 
+const DEFAULT_DATA = {
+  from: "",
+  subject: "",
+  message: "",
+};
+
 export default function ContactForm() {
-  const [form, setForm] = useState<Form>({
-    from: "",
-    subject: "",
-    message: "",
-  });
+  const [form, setForm] = useState<Form>(DEFAULT_DATA);
   const [banner, setBanner] = useState<BannerData | null>(null);
 
   const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,11 +27,25 @@ export default function ContactForm() {
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(form);
-    setBanner({ message: "성공했어!!", state: "success" });
-    setTimeout(() => {
-      setBanner(null);
-    }, 3000);
+    sendContactEmail(form) //
+      .then(() => {
+        setBanner({
+          message: "메일을 성공적으로 보냈습니다.",
+          state: "success",
+        });
+        setForm(DEFAULT_DATA);
+      })
+      .catch(() => {
+        setBanner({
+          message: "메일전송에 실패했습니다. 다시 시도해 주세요.",
+          state: "error",
+        });
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setBanner(null);
+        }, 3000);
+      });
   };
 
   return (
---
+++ src/service/contact.ts
@@ -0,0 +1,18 @@
+import { EmailData } from "./email";
+
+export async function sendContactEmail(email: EmailData) {
+  // 우리 API Route에 이메일 전송을 위한 요청을 보냄 (fetch)
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(email),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "서버 요청에 실패함 😂");
+  }
+  return data;
+}
