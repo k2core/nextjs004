b873bfa3c4f754922910042da1ce3e7b45156ec6
k2core <7179062@gmail.com>
2023-07-09 07:34:51 +0900

    이메일 보내기(2) - 서버 라우트 (완성)
    지메일을 설정하고, 실질적으로 메일을 보내는 함수
    
    Changes to be committed:
    modified:   package.json
    modified:   src/app/api/contact/route.ts
    modified:   src/service/email.ts
    modified:   yarn.lock

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from "@/service/email";
 import * as yup from "yup";
 
 const bodySchema = yup.object().shape({
@@ -7,10 +8,24 @@ const bodySchema = yup.object().shape({
 });
 
 export async function POST(req: Request) {
-  const body = req.json();
-  if (!bodySchema.isValidSync(req.body)) {
-    return new Response("유효하지 않은 포맷", { status: 400 });
+  const body = await req.json();
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(JSON.stringify({ message: "메일 전송에 실패함!" }), {
+      status: 400,
+    });
   }
-  const { from, subject, message } = req.body;
-  // 노드메일러를 이용해서 메일을 전송하면 됨
+
+  return sendEmail(body) //
+    .then(
+      () =>
+        new Response(JSON.stringify({ message: "메일을 성공적으로 보냈음" }), {
+          status: 200,
+        })
+    )
+    .catch((error) => {
+      console.log(error);
+      return new Response(JSON.stringify({ message: "메일 전송에 실패함!" }), {
+        status: 500,
+      });
+    });
 }
