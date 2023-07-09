fda3399117a5f3977663241c64ff9fc276f6b60b
k2core <7179062@gmail.com>
2023-07-09 23:29:56 +0900

    (최종)성능 개선 및 점검
    불필요한 파일 삭제
    cache 사용
    generateStaticParams 함수
    
    Changes to be committed:
    deleted:    public/next.svg
    deleted:    public/vercel.svg
    modified:   src/app/posts/[slug]/page.tsx
    modified:   src/service/posts.ts

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import AdjacentPostCard from "@/components/AdjacentPostCard";
 import PostContent from "@/components/PostContent";
-import { getPostData } from "@/service/posts";
+import { getFeaturedPosts, getPostData } from "@/service/posts";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -40,4 +40,11 @@ export default async function PostPage({ params: { slug } }: Props) {
       </section>
     </article>
   );
+}
+
+export async function generateStaticParams() {
+  const posts = await getFeaturedPosts();
+  return posts.map((post) => ({
+    slug: post.path,
+  }));
 }
