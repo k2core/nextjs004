1bf5c6356c84770e780bf6b4e4c36c8ee647a303
k2core <7179062@gmail.com>
2024-05-20 23:37:25 +0900

    0520 메시지2337

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,107 +5,109 @@ import Banner, { BannerData } from "./Banner";
 import { sendContactEmail } from "@/service/contact";
 
 type Form = {
-  from: string;
-  subject: string;
-  message: string;
+    from: string;
+    subject: string;
+    message: string;
 };
 
 const DEFAULT_DATA = {
-  from: "",
-  subject: "",
-  message: "",
+    from: "",
+    subject: "",
+    message: "",
 };
 
 export default function ContactForm() {
-  const [form, setForm] = useState<Form>(DEFAULT_DATA);
-  const [banner, setBanner] = useState<BannerData | null>(null);
+    const [form, setForm] = useState<Form>(DEFAULT_DATA);
+    const [banner, setBanner] = useState<BannerData | null>(null);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  };
+    const onChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { name, value } = e.target;
+        setForm((prev) => ({ ...prev, [name]: value }));
+    };
 
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    sendContactEmail(form) //
-      .then((resp) => {
-        console.log(resp);
-        setBanner({
-          message: resp.message,
-          state: "success",
-        });
-        setForm(DEFAULT_DATA);
-      })
-      .catch(() => {
-        setBanner({
-          message: "메일전송에 실패했습니다. 다시 시도해 주세요.",
-          state: "error",
-        });
-      })
-      .finally(() => {
-        setTimeout(() => {
-          setBanner(null);
-        }, 10000);
-      });
-  };
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        sendContactEmail(form) //
+            .then((resp) => {
+                console.log(resp);
+                setBanner({
+                    message: resp.message,
+                    state: "success",
+                });
+                setForm(DEFAULT_DATA);
+            })
+            .catch(() => {
+                setBanner({
+                    message: "메일전송에 실패했습니다. 다시 시도해 주세요.",
+                    state: "error",
+                });
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    setBanner(null);
+                }, 10000);
+            });
+    };
 
-  const sentDate = new Date(2023, 9 - 1, 7);
-  const today = new Date();
-  const display =
-    sentDate.getFullYear() === today.getFullYear() &&
-    sentDate.getMonth() === today.getMonth() &&
-    sentDate.getDate() === today.getDate();
+    const sentDate = new Date(2023, 9 - 1, 7);
+    const today = new Date();
+    const display =
+        sentDate.getFullYear() === today.getFullYear() &&
+        sentDate.getMonth() === today.getMonth() &&
+        sentDate.getDate() === today.getDate();
 
-  return (
-    <section className="w-full max-w-md">
-      {banner && <Banner banner={banner} />}
-      <form
-        onSubmit={onSubmit}
-        className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl"
-      >
-        <label htmlFor="from" className="font-semibold text-white">
-          Your Email
-        </label>
-        <input
-          type="email"
-          id="from"
-          name="from"
-          required
-          autoFocus
-          value={form.from}
-          onChange={onChange}
-        />
-        <label htmlFor="subject" className="font-semibold text-white">
-          Subject
-        </label>
-        <input
-          type="text"
-          id="subject"
-          name="subject"
-          required
-          value={form.subject}
-          onChange={onChange}
-        />
-        <label htmlFor="message" className="font-semibold text-white">
-          Message(ldicjdw)
-        </label>
-        <textarea
-          rows={5}
-          id="message"
-          name="message"
-          required
-          value={form.message}
-          onChange={onChange}
-          placeholder={
-            display || !display // 😍🤩😭🥯🥕🥖🍠 사랑해~❤️숨!
-              ? "(22:43)타고 출발!! 우리 자기 일찍 자요^^ 사랑해 쪼옥 '잘자요 사랑해 굿나잇 쪼옥' 예뻐예뻐 겁나 예뻐^^ 사랑해~❤️"
-              : "사랑해~❤️"
-          }
-        />
-        <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">
-          Submit
-        </button>
-      </form>
-    </section>
-  );
+    return (
+        <section className="w-full max-w-md">
+            {banner && <Banner banner={banner} />}
+            <form
+                onSubmit={onSubmit}
+                className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl"
+            >
+                <label htmlFor="from" className="font-semibold text-white">
+                    Your Email
+                </label>
+                <input
+                    type="email"
+                    id="from"
+                    name="from"
+                    required
+                    autoFocus
+                    value={form.from}
+                    onChange={onChange}
+                />
+                <label htmlFor="subject" className="font-semibold text-white">
+                    Subject
+                </label>
+                <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    required
+                    value={form.subject}
+                    onChange={onChange}
+                />
+                <label htmlFor="message" className="font-semibold text-white">
+                    Message(ldicjdw)
+                </label>
+                <textarea
+                    rows={5}
+                    id="message"
+                    name="message"
+                    required
+                    value={form.message}
+                    onChange={onChange}
+                    placeholder={
+                        display || !display // 😍🤩😭🥯🥕🥖🍠 사랑해~❤️숨!
+                            ? "(23:36)이수기둘. 오늘은 푹 자기를^^ 굿나잇 쪼옥!! (22:43)타고 출발!! 우리 자기 일찍 자요^^ 사랑해 쪼옥 '잘자요 사랑해 굿나잇 쪼옥' 예뻐예뻐 겁나 예뻐^^ 사랑해~❤️"
+                            : "사랑해~❤️"
+                    }
+                />
+                <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
 }
